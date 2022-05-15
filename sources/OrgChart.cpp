a5697b6a2e57a672a9006163ebe0e50962746d75
noamya-sh <noamyashany@gmail.com>
2022-05-15 18:25:13 +0300

    add func

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -1,7 +1,10 @@
 #include "OrgChart.hpp"
+#include <queue>
 namespace ariel{
+
     OrgChart& OrgChart::add_root(string s) {
-        m_first = new Node(s, nullptr);
+        root = new Node(s);
+        this->toSearch.push_back(root);
         return *this;
     }
 
@@ -10,31 +13,72 @@ namespace ariel{
     }
 
     OrgChart& OrgChart::add_sub(string s1, string s2) {
+        Node *f = search(s1);
+        if (f == nullptr){
+            throw runtime_error("no exist father");
+        }
+        Node *b = new Node(s2);
+        b->father = f;
+        f->boys.push_back(b);
         return *this;
     }
 
     OrgChart::iterator OrgChart::begin_level_order() {
-        return OrgChart::iterator();
+        return OrgChart::iterator(LEVEL);
     }
 
     OrgChart::iterator OrgChart::end_level_order() {
-        return OrgChart::iterator();
+        iterator it = OrgChart::iterator(LEVEL);
+
+        return it;
     }
 
     OrgChart::iterator OrgChart::begin_reverse_order() {
-        return OrgChart::iterator();
+        return OrgChart::iterator(REVERSE_LEVEL);
     }
 
-    OrgChart::iterator OrgChart::reverse_order() {
-        return OrgChart::iterator();
+    OrgChart::iterator OrgChart::end_reverse_order() {
+        return OrgChart::iterator(REVERSE_LEVEL);
     }
 
     OrgChart::iterator OrgChart::begin_preorder() {
-        return OrgChart::iterator();
+        return OrgChart::iterator(PREORDER);
     }
 
     OrgChart::iterator OrgChart::end_preorder() {
-        return OrgChart::iterator();
+        return OrgChart::iterator(PREORDER);
+    }
+
+    void OrgChart::bfs() {
+        vector<string> vec;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()){
+            Node* n = q.front();
+            vec.push_back(n->value);
+            //for: all nodes descents
+            q.pop();
+        }
+    }
+
+    OrgChart::Node *OrgChart::search(string s) {
+        for (Node *n:toSearch) {
+            if (n->value == s){
+                return n;
+            }
+        }
+        return nullptr;
+    }
+
+    string OrgChart::get_father(string &boy) {
+        Node *n = search(boy);
+        if (n == nullptr){
+            throw runtime_error("no exist this input");
+        }
+        if (n==root){
+            return "this root";
+        }
+        return n->father->value;
     }
 }
 
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -3,53 +3,65 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+
 using namespace std;
 namespace ariel{
-
+    enum type_it{
+        LEVEL,REVERSE_LEVEL,PREORDER
+    };
     class OrgChart{
     private:
         struct Node {
-            string m_value;
-            Node* m_next;
-            Node(string &v, Node* n)
-                    : m_value(v), m_next(n) {
+            string value;
+            vector <Node*> boys;
+            Node* father;
+            Node(string &v)
+                    : value(v){
             }
         };
+        Node* search(string s);
+        vector<Node*> toSearch;
 
     public:
-        OrgChart() : m_first(nullptr) {
-        }
+        OrgChart() : root(nullptr) {}
         ~OrgChart(){
-            delete m_first;
+            for (Node *n:toSearch) {
+                delete n;
+            }
         }
         OrgChart& add_root(string s);
         friend ostream &operator<<(ostream &output, const OrgChart &orgChart);
         OrgChart& add_sub(string s1, string s2);
-        Node* m_first;
-
+        Node* root;
+        void bfs();
         class iterator {
 
         private:
             Node* current;
+            type_it type = LEVEL;
 
         public:
-            iterator(Node* ptr = nullptr)
-                    : current(ptr) {
+            iterator(type_it t,Node* ptr = nullptr)
+                    : type(t),current(ptr) {
             }
 
             string& operator*() const {
                 //return *current;
-                return current->m_value;
+                return current->value;
             }
 
             string* operator->() const {
-                return &(current->m_value);
+                return &(current->value);
             }
 
             // ++i;
             iterator& operator++() {
                 //++current;
-                current = current->m_next;
+                if (type == LEVEL){}
+                else if (type == REVERSE_LEVEL){
+                }
+                else{}
+//                current = current->m_next;
                 return *this;
             }
 
@@ -57,7 +69,7 @@ namespace ariel{
             // Usually iterators are passed by value and not by const& as they are small.
             iterator operator++(int) {
                 iterator tmp= *this;
-                current= current->m_next;
+//                current= current->m_next;
                 return tmp;
             }
 
@@ -69,26 +81,24 @@ namespace ariel{
                 return current != it.current;
             }
             size_t size(){
-                return current->m_value.length();
+                return current->value.length();
             }
         };  // END OF CLASS ITERATOR
-        iterator begin() const {
-            // return &(m_first->m_value);
-            return iterator{m_first};
-        }
-//        static int size() {
-//            return 0;
+//        iterator begin() const {
+//            // return &(root->value);
+//            return iterator{LEVEL,root};
 //        }
-        static iterator end() {
-            // return nullptr;
-            return iterator{nullptr};
-        }
+//        static iterator end() {
+//            // return nullptr;
+//            return iterator{LEVEL,nullptr};
+//        }
         iterator begin_level_order();
         iterator end_level_order();
         iterator begin_reverse_order();
-        iterator reverse_order();
+        iterator end_reverse_order();
         iterator begin_preorder();
         iterator end_preorder();
+        string get_father(string &boy);
     };
 }
 #endif //ORGCHART_A_ORGCHART_H
